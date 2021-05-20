8f3380e585d4ea0c3037f87755f6bb05811d7b61
Nikita Bezdolniy <gooverdian@gmail.com>
2021-05-20 12:39:06 +0300

    HH-129187 Up lib versions

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
+const strip = require('strip-comments');
 const { transformFileSync } = require('@babel/core');
 
 const plugin = path.join(__dirname, '../src/index.js');
@@ -10,18 +11,22 @@ const fixturesDirWithoutEnv = path.join(__dirname, 'fixtures/withoutEnv');
 const trim = (str) => str.replace(/^\s+|\s+$/, '');
 
 const transformFile = (filename, isEnvPreset) =>
-    transformFileSync(filename, {
-        babelrc: false,
-        plugins: [plugin],
-        presets: ['@babel/preset-react'].concat(isEnvPreset ? ['@babel/preset-env'] : []),
-    }).code;
+    // @babel/preset-react adds pure annotation block comments to components, better to strip it
+    // https://github.com/babel/babel/tree/main/packages/babel-helper-annotate-as-pure
+    strip.block(
+        transformFileSync(filename, {
+            babelrc: false,
+            plugins: [plugin],
+            presets: ['@babel/preset-react'].concat(isEnvPreset ? ['@babel/preset-env'] : []),
+        }).code
+    );
 
 describe('@hh.ru/babel-plugin-react-source adds source line in react elements with env presets', () => {
     fs.readdirSync(fixturesDirEnv).forEach(function(fixture) {
         const actual = transformFile(path.join(fixturesDirEnv, fixture, 'input.js'), true);
         const expected = fs.readFileSync(path.join(fixturesDirEnv, fixture, 'expected.js'), 'utf8');
         it(path.basename(fixture), function () {
-            assert.equal(trim(actual), trim(expected));
+            assert.strictEqual(trim(actual), trim(expected));
         });
     });
 });
@@ -31,7 +36,7 @@ describe('@hh.ru/babel-plugin-react-source adds source line in react elements wi
         const actual = transformFile(path.join(fixturesDirWithoutEnv, fixture, 'input.js'), false);
         const expected = fs.readFileSync(path.join(fixturesDirWithoutEnv, fixture, 'expected.js'), 'utf8');
         it(path.basename(fixture), function () {
-            assert.equal(trim(actual), trim(expected));
+            assert.strictEqual(trim(actual), trim(expected));
         });
     });
 });
